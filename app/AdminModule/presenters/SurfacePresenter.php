db8e40be43356a293d33d4c5daa57342e06884e5
Jan Šedivý <honza.seeda@gmail.com>
2019-04-20 13:04:39 +0200

    Re #7336 Mazání typů

--- app/AdminModule/presenters/SurfacePresenter.php
+++ app/AdminModule/presenters/SurfacePresenter.php
@@ -5,6 +5,8 @@ namespace App\AdminModule\Presenters;
 
 use App\AdminModule\Components\ISurfaceTypeEditFormFactory;
 use App\AdminModule\Components\ISurfaceTypeGridFactory;
+use App\Enum\EFlashMessage;
+use App\Model\Repository\SurfaceTypeRepository;
 
 class SurfacePresenter extends BaseAdminPresenter
 {
@@ -14,22 +16,28 @@ class SurfacePresenter extends BaseAdminPresenter
     /** @var ISurfaceTypeEditFormFactory */
     private $surfaceTypeEditFormFactory;
 
+    /** @var SurfaceTypeRepository */
+    private $surfaceTypeRepository;
+
     private $typeId;
 
     /**
      * SurfacePresenter constructor.
      * @param ISurfaceTypeGridFactory $surfaceTypeGridFactory
      * @param ISurfaceTypeEditFormFactory $surfaceTypeEditFormFactory
+     * @param SurfaceTypeRepository $surfaceTypeRepository
      */
     public function __construct(
         ISurfaceTypeGridFactory $surfaceTypeGridFactory,
-        ISurfaceTypeEditFormFactory $surfaceTypeEditFormFactory
+        ISurfaceTypeEditFormFactory $surfaceTypeEditFormFactory,
+        SurfaceTypeRepository $surfaceTypeRepository
     )
     {
         parent::__construct();
 
         $this->surfaceTypeGridFactory = $surfaceTypeGridFactory;
         $this->surfaceTypeEditFormFactory = $surfaceTypeEditFormFactory;
+        $this->surfaceTypeRepository = $surfaceTypeRepository;
     }
 
     public function actionAddType()
@@ -40,11 +48,20 @@ class SurfacePresenter extends BaseAdminPresenter
     public function actionEditType(int $id)
     {
         $this->typeId = $id;
+        $this->template->id = $id;
     }
 
     public function actionDeleteType(int $id)
     {
-
+        if($this->surfaceTypeRepository->delete($id))
+        {
+            $this->flashMessage('Surface type was deleted.', EFlashMessage::SUCCESS);
+        }
+        else
+        {
+            $this->flashMessage('Surface type was not found.', EFlashMessage::ERROR);
+        }
+        $this->redirect('Surface:');
     }
 
     public function createComponentSurfaceTypeGrid()
